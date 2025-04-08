7af044faa97e1c3a79189ea7f475f223c67954a7
tienmeomeo <tiennn107@gmail.com>
2025-04-08 07:15:21 +0700

    v

--- resources/views/components/web-layout.blade.php
+++ resources/views/components/web-layout.blade.php
@@ -95,7 +95,7 @@
 					<!-- LOGO -->
 					<div class="col-md-3">
 						<div class="header-logo">
-							<a href="{{ url('/') }}" class="logo">
+							<a href="#" class="logo">
 								<img src="{{ asset('img/logo.png') }}" alt="Logo">
 							</a>
 						</div>
@@ -106,6 +106,11 @@
 					<div class="col-md-6">
 						<div class="header-search">
 							<form>
+								<select class="input-select">
+									<option value="0">All Categories</option>
+									<option value="1">Category 01</option>
+									<option value="2">Category 02</option>
+								</select>
 								<input class="input" placeholder="Bạn cần tìm gì?">
 								<button class="search-btn">Tìm kiếm</button>
 							</form>
@@ -172,32 +177,10 @@
 			<div id="responsive-nav">
 				<!-- NAV -->
 				<ul class="main-nav nav navbar-nav">
-					<li class="active"><a href="{{ url('/') }}">Trang chủ</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Sản phẩm <i class="fa fa-caret-down"></i></a>
-						<ul class="dropdown-menu">
-							@foreach($categories as $category)
-								<li>
-									<a href="{{ route('store.index', ['category' => $category->id]) }}">
-										{{ $category->ten_loai_san_pham }}
-									</a>
-                				</li>
-            				@endforeach
-        				</ul>
-    				</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Thương hiệu <i class="fa fa-caret-down"></i></a>
-						<ul class="dropdown-menu">
-							@foreach($brands as $brand)
-								<li>
-									<a href="{{ route('store.index', ['brand' => $brand->id]) }}">
-										{{ $brand->ten_nha_san_xuat }}
-									</a>
-								</li>
-							@endforeach
-						</ul>
-					</li>
-					<li><a href="{{ url('/') }}#new-products">Sản phẩm mới nhất</a></li>
+					<li class="active"><a href="#">Trang chủ</a></li>
+					<li><a href="#">Sản phẩm</a></li>
+					<li><a href="#">Thương hiệu</a></li>
+					<li><a href="#">Phụ kiện công nghệ</a></li>
 				</ul>
 				<!-- /NAV -->
 			</div>
@@ -215,7 +198,7 @@
 		{{$slot}}
 	</div>
 	<!-- /SECTION -->
-	
+
 	<!-- NEWSLETTER -->
 	<div id="newsletter" class="section">
 		<!-- container -->
@@ -383,7 +366,7 @@
 
 		/* Kích thước của dropdown */
 		.dropdown-toggle {
-			font-size: 15px;
+			font-size: 18px;
 		}
 
 		/* Thêm hiệu ứng cho dropdown */
@@ -398,50 +381,3 @@
 			visibility: visible;
 		}
 	</style>
-
-
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const hash = window.location.hash;
-        if (hash) {
-            const target = document.querySelector(hash);
-            if (target) {
-                setTimeout(() => {
-                    target.scrollIntoView({ behavior: "smooth" });
-                }, 100); // Delay để chắc chắn phần tử đã render
-            }
-        }
-    });
-</script>
-<script>
-$(document).ready(function() {
-    // Khi người dùng nhấn nút "Quick View"
-    $('.quick-view').on('click', function() {
-        var productId = $(this).data('id'); // Lấy id sản phẩm
-
-        // Gửi AJAX request để lấy thông tin sản phẩm
-        $.ajax({
-            url: '/quick-view/' + productId, // Địa chỉ API để lấy dữ liệu sản phẩm
-            method: 'GET',
-            success: function(response) {
-                // Hiển thị thông tin sản phẩm vào modal
-                $('#quick-view-content').html(response);
-                $('#quick-view-modal').show(); // Hiển thị modal
-            },
-            error: function() {
-                alert('Không thể tải thông tin sản phẩm.');
-            }
-        });
-    });
-
-    // Đóng modal khi nhấn vào dấu "X"
-    $('.close').on('click', function() {
-        $('#quick-view-modal').hide();
-    });
-});
-</script>
-<x-quick-view-modal/>
-
-
-
-
